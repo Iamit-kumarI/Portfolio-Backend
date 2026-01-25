1c0c5f0601d0ff2e6a19d48619e7ffe9a8b13b64
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-26 00:11:40 +0530

    done

--- profile-backend/src/main/java/com/portfolio/profile_backend/service/EmailService.java
+++ profile-backend/src/main/java/com/portfolio/profile_backend/service/EmailService.java
@@ -2,6 +2,7 @@ package com.portfolio.profile_backend.service;
 
 import com.portfolio.profile_backend.dto.ContactRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,16 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String ownerEmail;
+
     public void sendContactEmail(ContactRequest request) {
 
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo("YOUR_EMAIL@gmail.com"); // where you want to receive mail
+        message.setFrom(ownerEmail);               // MUST
+        message.setTo(ownerEmail);                 // MUST
+        message.setReplyTo(request.getEmail());    // MUST
         message.setSubject("New Contact Form Message");
 
         message.setText(
